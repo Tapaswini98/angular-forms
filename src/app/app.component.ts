74f0483049c2b9124af816f18869caaf490e1769
Tapaswini kar <kar.tapaswini1111@gmail.com>
2020-08-22 01:01:49 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder , FormGroup , Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-forms';
+  
+  rForm: FormGroup;
+  post:any;
+  name:string='';
+  about:string=' '; 
+  validate:string='';
+  select:string='';
+  password:string='';
+  titleAlert:string='This field is required!'
+  
+
+  constructor( private fb: FormBuilder){
+    this.rForm =fb.group({
+      'name':[null,Validators.required],
+      'about':[null,Validators.compose([Validators.required ,Validators.maxLength(100)])],
+      'validate':[null,Validators.required],
+      'select':[null,Validators.required],
+      'password':''
+    });
+  }
+  addPost(post){
+    this.name =post.name;
+    this.about =post.about;
+    this.validate=post.validate;
+    this.select=post.select;
+   
+  }
 }
